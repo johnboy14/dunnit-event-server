fe9fc3b1023641ee5f41a95311945b12a23b2196
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-08-24 13:45:02 +0100

    Send a dummy message because the server is not calling out to google APIs yet

--- src/dunnit/redis/component.clj
+++ src/dunnit/redis/component.clj
@@ -1,11 +1,17 @@
 (ns dunnit.redis.component
   (:require [com.stuartsierra.component :as comp]
             [taoensso.carmine :as car :refer (wcar)]
-            [clojure.core.async :as c]))
+            [clojure.core.async :as c]
+            [cheshire.core :as ch]))
+
+(defn retrieve-event-details [notification]
+  (let [{:keys [emailAddress historyId]} (ch/parse-string notification true)]
+    (-> {:emailAddress emailAddress :historyId historyId :message "DUNNIT GOES HERE!!!"}
+        (ch/generate-string))))
 
 (defn start-pushing [config channel]
   (c/go-loop []
-    (let [val (c/<!! channel)]
+    (let [val (retrieve-event-details (c/<!! channel))]
       (println val)
       (if-not (nil? val)
         (do (car/wcar config (car/publish "dunnit" val))
