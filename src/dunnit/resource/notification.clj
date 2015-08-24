45a9f5c62b2fc6f977cd63dc5b13d2d2389c28b4
Ervine, John(e20042) <John.Ervine@cmegroup.com>
2015-08-24 14:12:16 +0100

    Added a handler for handling an invalid json payload.

--- src/dunnit/resource/notification.clj
+++ src/dunnit/resource/notification.clj
@@ -11,4 +11,5 @@
  :malformed? (fn [ctx] (validate-notification (retrieve-body payload)))
  :allowed-methods [:post]
  :post! (fn [ctx] (c/>!! channel (retrieve-body payload)))
- :handle-created "")
+ :handle-created ""
+ :handle-malformed "Payload is Invalid")
